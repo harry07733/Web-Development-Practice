1521795e5839f8765c11d1f4dbc61eca95d93617
harry07733 <shaswatswain53@gmail.com>
2026-03-25 01:15:08 +0530

    Initial commit with organized projects

---
+++ Random joke page/rjp.js
@@ -0,0 +1,18 @@
+let jokeTextEl = document.getElementById("jokeText");
+let jokeBtnEl = document.getElementById("jokeBtn");
+let spinnerEl = document.getElementById("spinner");
+
+jokeBtnEl.onclick = function() {
+    spinnerEl.classList.remove("d-none");
+    let options = {
+        method: "GET",
+    };
+    let requestedURL = "https://apis.ccbp.in/jokes/random";
+    fetch(requestedURL, options)
+        .then(response => response.json())
+        .then(jsonData => {
+            spinnerEl.classList.add("d-none");
+            jokeTextEl.textContent = jsonData.value;
+        });
+
+}
